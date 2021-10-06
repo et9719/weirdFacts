a7ba435c552e6b5c20f0a907b13811a72f8a9cef
et9719 <ella-t@live.co.uk>
2021-10-06 12:43:13 +0000

    Add functions for the give feedback button and the exy button on the feedback form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 let startSection = document.getElementById('startSection');
 let instructionsSection = document.getElementById('instructions');
+let feedbackSection = document.getElementById('form');
 
 let instructionsBtn = document.getElementById('instructionsBtn');
-let exitBtn = document.getElementById('exit');
+let exitBtn = document.getElementById('exitInstructions');
+let giveFeedbackBtn = document.getElementById('feedback');
+let exitFeedbackBtn = document.getElementById('exitFeedback');
+
 
 // Show the instructions when instructions button is clicked.
 instructionsBtn.addEventListener('click', showInstructions);
@@ -15,4 +19,16 @@ exitBtn.addEventListener('click', exitInstructions);
 function exitInstructions() {
     startSection.classList.remove('hide');
     instructionsSection.classList.add('hide');
+}
+// Show the Feedback form when give feedback button is clicked.
+giveFeedbackBtn.addEventListener('click', showFeedbackForm);
+function showFeedbackForm() {
+    startSection.classList.add('hide');
+    feedbackSection.classList.remove('hide');
+}
+// Exit the instructions when exit button is clicked.
+exitFeedbackBtn.addEventListener('click', exitFeedback);
+function exitFeedback() {
+    startSection.classList.remove('hide');
+    feedbackSection.classList.add('hide');
 }
